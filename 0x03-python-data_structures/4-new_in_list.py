213246e117bda63f61ac1dbae681d010b67ec6da
chebt <chebetdaniel590@gmail.com>
2022-09-26 19:14:37 -0700

    python DS

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-def no_c(my_str):
-    """
-    Returns a copy of my_str without c or C
-    Args:
-        my_str - the string to filter
-    """
-    return "".join(filter(lambda x: x not in 'cC', my_str))
+def new_in_list(my_list, idx, element):
+    copy = my_list[:]
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+    copy[idx] = element
+    return copy
